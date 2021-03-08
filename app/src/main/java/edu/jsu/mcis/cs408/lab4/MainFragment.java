7aca134f18716cda86a1f1edc756eeb62e0dc3a7
bcochran210 <46578870+bcochran210@users.noreply.github.com>
2021-03-08 05:40:35 -0600

    Error Messages Added

--- app/src/main/java/edu/jsu/mcis/cs408/lab4/MainFragment.java
+++ app/src/main/java/edu/jsu/mcis/cs408/lab4/MainFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
@@ -35,9 +37,38 @@ public class MainFragment extends Fragment {
     }
 
     public void onClick(View view) {
-        double tip = Double.parseDouble("0." + tipPercentage.getText().toString());
-        int bill = Integer.parseInt(billTotal.getText().toString());
-        int diners = Integer.parseInt(numDiners.getText().toString());
+
+        double tip = 0.0;
+        double bill = 0.0;
+        int diners = 0;
+
+        Toast emptyTip = Toast.makeText(getActivity(), "Tip left blank.", Toast.LENGTH_SHORT);
+        Toast emptyBill = Toast.makeText(getActivity(), "Bill left blank.", Toast.LENGTH_SHORT);
+        Toast emptyDiners = Toast.makeText(getActivity(), "Diners left blank.", Toast.LENGTH_SHORT);
+
+        if (tipPercentage.getText().toString() == "" || tipPercentage.getText().toString().isEmpty()) {
+            emptyTip.show();
+        } else {
+            tip = Double.parseDouble(tipPercentage.getText().toString()) / 100;
+        }
+
+        if (billTotal.getText().toString() == "" || billTotal.getText().toString().isEmpty()) {
+            emptyBill.show();
+        } else {
+            bill = Integer.parseInt(billTotal.getText().toString());
+        }
+
+        if (numDiners.getText().toString() == "" || numDiners.getText().toString().isEmpty()) {
+            emptyDiners.show();
+        } else {
+            diners = Integer.parseInt(numDiners.getText().toString());
+        }
+
+        if (tip == 0.0 || bill == 0.0 || diners == 0.0) {
+            Toast errorMsg = Toast.makeText(getActivity(), "0 is not a valid input.", Toast.LENGTH_SHORT);
+            errorMsg.show();
+        }
+
         double newTotal = bill + (bill * tip);
         double totalPerDiner = newTotal / diners;
         double roundedTotal = Math.ceil(totalPerDiner * 100.0) / 100.0;
